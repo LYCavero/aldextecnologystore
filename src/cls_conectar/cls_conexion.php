7a109fcd354ede6740b232777aa47dfcfcfab1ce
Luis Yanpier Dev <caveroluis_17@outlook.com>
2025-08-11 21:05:09 -0500

    Primer commit del repositorio

--- src/cls_conectar/cls_conexion.php
+++ src/cls_conectar/cls_conexion.php
@@ -1,11 +1,19 @@
 <?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
     class Conexion{
         function getConectar(){
             //variakbes 
-            $server="cont-db-mysql-dev:3306";
-            $login="root";
-            $clave="root";
-            $bd="bd_aldex_tecnology_store";
+            $server=getenv("DB_HOST");
+            $login=getenv("DB_USER");
+            $clave=getenv("DB_PASS");
+            $bd=getenv("DB_NAME");
             //function
             $cn=mysqli_connect($server,$login,$clave,$bd);
             return $cn;
